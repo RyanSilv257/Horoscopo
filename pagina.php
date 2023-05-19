a2b0667cfddc06ecf814a6116bafbdc1aff14e6d
RyanSilv257 <130668947+RyanSilv257@users.noreply.github.com>
2023-05-19 16:35:59 -0300

    commit do seu pai

---
+++ pagina.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Horóscopo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="pagina0.php" method="get">
+        <label for="data">Informe a data do seu nascimento:</label><br>
+        <input type="date" name="data">
+        <input type="submit">
+    </form>
+</body>
+</html>
