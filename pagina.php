0c324ede18a9a0d568363955a61d94fa8953c0d6
RyanSilv257 <ryan.nascimento257@gmail.com>
2023-05-27 19:47:47 -0300

    commit

--- pagina.php
+++ pagina.php
@@ -5,11 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Horóscopo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
 <body>
     <form action="pagina0.php" method="get">
-        <label for="data">Informe a data do seu nascimento:</label><br>
+        <label for="lblData">Informe a data do seu nascimento:</label><br>
         <input type="date" name="data">
         <input type="submit">
     </form>
